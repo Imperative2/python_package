1eeda0b4893886804104c09d07bfdaacf98603ae
Imperative2 <nie335@gmail.com>
2019-05-28 07:48:38 +0200

    version 0.1

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='python_package',
+    version='0.1',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='An example python package',
+    long_description=open('README.md').read(),
+    install_requires=['numpy'],
+    url='https://github.com/Imperative2/python_package',
+    author='Karol masluch',
+    author_email='myemail@example.com'
+)
