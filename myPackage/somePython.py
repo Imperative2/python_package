9557028a853ddf900d955a48742dbdcfb99a65f4
Imperative2 <nie335@gmail.com>
2019-05-28 08:11:14 +0200

    3

--- myPackage/somePython.py
+++ myPackage/somePython.py
@@ -1,10 +1,6 @@
 import numpy
 
 def calc(temp):
-    '''
-    takes a temperature `temp` in fahrenheit and returns it in Kelvin
-    '''
 
-    kelvin = 5./9. * (temp - 32.) + 273.15
 
-    return kelvin
+    return temp
